93a6193c19792484db3768d8a5fc2687dff55a05
Kyle Weekley <krw1619@g.rit.edu>
2021-03-27 15:20:06 -0400

    Player values update collector constantly, trash can moved to bottom middle of screen

--- NoTextBeach/Assets/Scripts/Player.cs
+++ NoTextBeach/Assets/Scripts/Player.cs
@@ -21,8 +21,6 @@ public class Player : MonoBehaviour
         moveSpeed = 4.0f; 
         collectRange = 1.0f;
         collectCapacity = 5;
-
-        UpdateCollectorValues();
     }
 
     // Update is called once per frame
@@ -30,6 +28,7 @@ public class Player : MonoBehaviour
     {
 
         FollowCursor();
+        UpdateCollectorValues();
     }
 
     //Player follows the cursor at a set speed
